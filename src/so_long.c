3bd50cfc20381c2ea6fc288a18762ea38a09cf9d
ssandova <ssandova@student.42malaga.com>
2024-10-07 13:35:15 +0200

    map loads on window

--- src/so_long.c
+++ src/so_long.c
@@ -26,51 +26,63 @@ void	load_map(t_mlxinfo *mlx)
 	int	j;
 
 	i = 0;
-	while (i <= mlx->map_info->height)
+	while (i < mlx->map_info->height)
 	{
 		j = 0;
-		while (j <= mlx->map_info->width)
+		while (j < mlx->map_info->width)
 		{
 			if (mlx->map_info->map_cont[i][j] == '1')
 				mlx_image_to_window(mlx->mlx, mlx->wall, j * 64, i * 64);
+			else if (mlx->map_info->map_cont[i][j] == '0')
+				mlx_image_to_window(mlx->mlx, mlx->empty, j * 64, i * 64);
+			else if (mlx->map_info->map_cont[i][j] == 'P')
+				mlx_image_to_window(mlx->mlx, mlx->player, j * 64, i * 64);
+			else if (mlx->map_info->map_cont[i][j] == 'E')
+				mlx_image_to_window(mlx->mlx, mlx->exit, j * 64, i * 64);
+			else if (mlx->map_info->map_cont[i][j] == 'C')
+				mlx_image_to_window(mlx->mlx, mlx->collectible, j * 64, i * 64);
 			j++;
 		}
 		i++;
 	}
 }
+mlx_image_t	*mlx_tex_to_img(mlx_t *mlx, mlx_image_t *img, char *p)
+{
+	mlx_texture_t	*texture;
+
+	texture = mlx_load_png(p);
+	if (!mlx || !texture)
+		return (NULL);
+	img = mlx_texture_to_image(mlx, texture);
+	return (img);
+}
 
 t_mlxinfo	*mlx_initialize(t_map *map)
 {
 	t_mlxinfo		*mlx;
-	mlx_texture_t	*img;
 
 	mlx = (t_mlxinfo *)ft_calloc(sizeof(t_mlxinfo), 1);
-	if (!mlx)
-		return (NULL);
 	mlx->map_info = map;
+
 	mlx->mlx = mlx_init(mlx->map_info->width * 64, mlx->map_info->height * 64, "so long", \
 		false);
-	img = mlx_load_png("textures/wall.png");
-	mlx->wall = mlx_texture_to_image(mlx->mlx, img);
-	// img = mlx_load_png("textures/player.png");
-	// mlx->player = mlx_texture_to_image(mlx->mlx, img);
-	// img = mlx_load_png("textures/exit.png");
-	// mlx->exit = mlx_texture_to_image(mlx->mlx, img);
-	// img = mlx_load_png("textures/empty.png");
-	// mlx->empty = mlx_texture_to_image(mlx->mlx, img);
-	// img = mlx_load_png("textures/collectible.png");
-	// mlx->collectible = mlx_texture_to_image(mlx->mlx, img);
-	// if (!mlx->collectible || !mlx->empty || !mlx->exit || !mlx->map_info || 
-	// 	!mlx->player || mlx->wall)
-	// 	return (error_sl(map, 6), NULL);
-	return (mlx);
+	if (!mlx || !mlx->mlx)
+		return (NULL);
 
+	mlx->wall = mlx_tex_to_img(mlx->mlx, mlx->wall, "textures/wall.png");
+	mlx->collectible = mlx_tex_to_img(mlx->mlx, mlx->collectible, "textures/collectible.png");
+	mlx->empty = mlx_tex_to_img(mlx->mlx, mlx->empty, "textures/empty.png");
+	mlx->exit = mlx_tex_to_img(mlx->mlx, mlx->exit, "textures/exit.png");
+	mlx->player = mlx_tex_to_img(mlx->mlx, mlx->player, "textures/player.png");
+	if (!mlx->collectible || !mlx->empty || !mlx->exit || !mlx->player || !mlx->wall)
+	 	return (error_sl(map, 6), NULL);
+	return (mlx);
 }
 
 int	main(int argc, char **argv)
 {
-	t_map		*map;
-	t_mlxinfo	*mlx;
+	t_map			*map;
+	t_mlxinfo		*mlx;
 
 	if ((argc != 2) || (!argv[1][0]))
 		return (error_sl(NULL, 0), 1);
@@ -79,10 +91,10 @@ int	main(int argc, char **argv)
 		return (1);
 	if (parse_map(argv[1], map))
 		return (1);
+
 	mlx = mlx_initialize(map);
-	if (!mlx )
-		return (1);
 	load_map(mlx);
+
 	mlx_loop(mlx->mlx);
 	mlx_terminate(mlx->mlx);
 	return (0);
